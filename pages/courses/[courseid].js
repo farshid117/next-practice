d2ee81b2ccfc6605a22b1c294ecd62d0c81582e3
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-09-09 12:01:11 +0330

    next-practice --> useRouter : add explain2

--- pages/courses/[courseid].js
+++ pages/courses/[courseid].js
@@ -1,4 +1,9 @@
+import {useRouter} from "next/router"
+
 export default function CourseDetails({children}) {
+
+    const route = useRouter()
+    console.log("route in courses: ", route.query);
     return (
         <section>
             <h1>CourseDetails</h1>
--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,4 +1,9 @@
+import { useRouter } from "next/router"
 export default function UserDetails({children}) {
+ 
+    const router = useRouter()
+    console.log("route in user: ", router);
+
     return (
         <section>
             <h1>UsersDetails</h1>
