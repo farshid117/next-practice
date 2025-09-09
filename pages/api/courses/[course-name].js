78932e4bc31d66612969188de48e0c43d61dd92a
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-09-09 10:42:33 +0330

    next-practice : adjust path

--- pages/api/courses/[course-name].js
+++
@@ -1,9 +0,0 @@
-export default function Course({children}) {
-    return (
-        <section>
-            <p>Hello Course page</p>
-        </section>
-    );
-}
-
-nalayout
--- pages/api/courses/index.js
+++
@@ -1,7 +0,0 @@
-export default function Courses({children}) {
-    return (
-        <section>
-            <h1>Courses</h1>
-        </section>
-    );
-}
