84671f5f57231dd52e446311f52611348c474e07
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-09-09 12:59:52 +0330

    next-practice --> adjuset 2 [course-short-name].js

--- pages/courses/[course-short-name].js
+++ pages/courses/[course-short-name].js
@@ -4,6 +4,7 @@ export default function CourseDetails({children}) {
 
     const route = useRouter()
     console.log("route in courses: ", route.query);
+    
     return (
         <section>
             <h1>Course Details({route.query["course-short-name"]})</h1>
