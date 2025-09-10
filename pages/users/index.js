fb4d6dc1fd3f00ac2cd1ed8d4ee6bbf61cac5f4d
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-09-10 10:04:50 +0330

    next-specialist --> navigate between route

--- pages/users/index.js
+++ pages/users/index.js
@@ -3,7 +3,10 @@ export default function User({children}) {
     return (
         <section>
             <h1>Usesrs List </h1>
-            <Link href="/users/12/userdetails">لیست دوره ها</Link> 
+           <div className="flex justify-center items-center gap-7 ">
+                <Link href="/users/12/userdetails" className=" text-red-500 btn btn-primary">لیست دوره ها</Link>
+                <Link href="../courses/react-js/sessions/20" className="btn btn-primary">لیست اپیزود</Link> 
+           </div>
         </section>
     );
 }
