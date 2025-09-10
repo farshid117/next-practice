ccfa5fb9ccf26cccbc47acc162d258ce93c20536
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-09-10 12:50:01 +0330

    next-practice: some Link exercice

--- pages/courses/[course-short-name]/index.js
+++ pages/courses/[course-short-name]/index.js
@@ -1,7 +1,12 @@
+import { useRouter } from "next/router";
+
 export default function CourseShortName({children}) {
+ 
+    const route = useRouter()
+
     return (
         <section>
-            <h1>Course ShortName Folder</h1>
+            <h1>Course of  : {route.query["course-short-name"]} </h1>
         </section>
     );
 }
--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,7 +1,27 @@
+import Link from "next/link";
+
 export default function Courses({children}) {
+ 
+    const courses = [
+        {id: 1 , title: "HTML" , ["course-short-name"]: "html" },
+        {id: 2 , title: "Css" , ["course-short-name"]: "css" },
+        {id: 3 , title: "Javascript" , ["course-short-name"]: "js" },
+    ]
+
     return (
         <section>
-            <h1>Courses</h1>
+            <h1>Courses : </h1>
+            <div className="flex justify-center items-center">
+                <ul>
+                    {
+                        courses.map(course => (
+                            <li key={course.id}>
+                                <Link href={`/courses/${course["course-short-name"]}`}>{course.title}</Link>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
         </section>
     );
 }
