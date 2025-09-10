8d0f5026407c2d4e718c6f3cfb90b31d9ab993b0
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-09-10 17:51:37 +0330

    next-practice: _app.js : add Header & Footer

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ function NotFound() {
   return (
     <>
           <h1 className='flex justify-center items-center w-[100vh] text-red-400'>Page NotFound</h1>
-          <p className=' text-center font-bold' style={{direction:"rtl"}}>لطفا آدرس را درست وارد کنید 😊</p>
+          <p className=' text-center font-bold ' style={{direction:"rtl"}}>لطفا آدرس را درست وارد کنید 😊</p>
     </>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+		<>
+			<h1 className='font-sans font-bold text-center  text-amber-200'  style={{fontSize:"3ren"}}> Header Section</h1>
+			<hr />
+			<Component {...pageProps} />
+			<hr />
+			<h1 className=' font-sans font-bold text-center text-amber-200' style={{fontSize:"3ren"}}>Footer Section</h1>
+		</>
+	);
 }
