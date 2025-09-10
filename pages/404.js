0948d792abe835cf39a9b281890cf307549a2d2b
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-09-10 17:10:00 +0330

    next-practice: add 404.js file

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <>
+          <h1 className='flex justify-center items-center w-[100vh] text-red-400'>Page NotFound</h1>
+          <p className=' text-center font-bold' style={{direction:"rtl"}}>لطفا آدرس را درست وارد کنید 😊</p>
+    </>
+  )
+}
+
+export default NotFound
