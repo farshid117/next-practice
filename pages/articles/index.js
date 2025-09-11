ffb08be8ea0c8a0f3237d93c2c728bd2e5af0443
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-09-11 18:13:38 +0330

    next-specialist: styled-jsx : add explain3

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,14 +1,29 @@
 import React from 'react'
-import styles from "./index.module.css"
-console.log("styles: ", styles);
+// import styles from "./index.module.css"
+// console.log("styles: ", styles);
 
 function Articles() {
-    let {textColor, title } = styles
-   
+  // let { textColor, title } = styles
+
   return (
-      <div className={title}>
-          <h1>Articles - index.js</h1>
-    </div>
+    <>
+      <div className="title">
+        <h1>Articles - index.js</h1>
+      </div>
+
+      <style jsx>
+        {`
+          h1{
+            color: red;
+          }
+          .title{
+            background: purple;
+          }
+      `}
+      </style>
+
+    </>
+
   )
 }
 
